9167cb9bb6cc81d95cdcc1bd4b922435805ab307
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-30 21:43:52 -0300

    gitignore folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({message: "ain duuu"});
+  return res.json({message: "teste"});
 });
 
 app.listen(3200);
